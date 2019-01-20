d7862a531ed5f3a359a0a0c6405493bcc56314dd
februarysea <857254373@qq.com>
2019-01-20 10:43:14 +0800

    add prediction plot function

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -34,6 +34,36 @@ model.fit(train_images, train_labels, epochs=5)  # 拟合
 
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 
-predictions = model.predict(test_images)
-print(predictions[0])
-print(np.argmax(predictions[0]))
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
